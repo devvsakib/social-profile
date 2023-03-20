27e586751e5e47ac5a0a12694d84a329fef46f3c
DevvSakib <devvsakib@gmail.com>
2023-03-20 21:37:52 +0600

    top profile section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import HeroSection from "../components/HeroSection/HeroSection.jsx"
-
+import TopProfileCard from "../components/Profile/TopProfileCard.jsx"
+import users from "../userdata.json"
 const Home = () => {
     return (
-        <div>
+        <section className="">
             <HeroSection />
-        </div>
+            <section className="my-32 bg-[url(assets/TopProfileShape.png)] bg-center bg-cover">
+                <div>
+                    <h2 className="text-xl font-semibold">Top Profile</h2>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-20 mt-10">
+                    {
+                        users ? users.slice(0,3).map((user, idx)=>(
+                            <TopProfileCard 
+                                user={user}
+                                id={idx}
+                            />
+                            )) : <h2>No Top Profile yet</h2>
+
+                    }
+                </div>
+            </section>
+        </section>
     )
 }
 
