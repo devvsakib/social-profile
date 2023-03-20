853e34b34584674b4975f7e2f4e6909c8e496ca1
DevvSakib <devvsakib@gmail.com>
2023-03-20 21:41:49 +0600

    all profile added

--- src/components/Profile/ProfileCard.jsx
+++ src/components/Profile/ProfileCard.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { FaTwitter, FaLinkedin, FaFacebook, FaInstagram, FaGithub } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
-const ProfileCard = () => {
+const ProfileCard = ({ user, id }) => {
   return (
-    <div>ProfileCard</div>
+    <div key={id} className="w-6/6 overflow-hidden rounded-lg cardbg">
+      <img className="h-60 object-cover w-full" src={user.profile_picture_url} alt="" />
+      <div className="px-3 pb-6">
+        <div className="flex justify-between my-5">
+          <h2 className="text-xl font-semibold">{user.fullname}</h2>
+          <button>
+            <Link>View</Link>
+          </button>
+        </div>
+        <div className="flex justify-center text-3xl gap-5 mt-10">
+          <a target={'_blank'} className="hover:text-[#1E0101] transition-all duration-200 ease-linear" href={user.social_media_links.twitter}><FaTwitter /></a>
+          <a target={'_blank'} className="hover:text-[#1E0101] transition-all duration-200 ease-linear" href={user.social_media_links.facebook}><FaFacebook /></a>
+          <a target={'_blank'} className="hover:text-[#1E0101] transition-all duration-200 ease-linear" href={user.social_media_links.github}><FaGithub /></a>
+          <a target={'_blank'} className="hover:text-[#1E0101] transition-all duration-200 ease-linear" href={user.social_media_links.instagram}><FaInstagram /></a>
+          <a target={'_blank'} className="hover:text-[#1E0101] transition-all duration-200 ease-linear" href={user.social_media_links.linkedin}><FaLinkedin /></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import HeroSection from "../components/HeroSection/HeroSection.jsx"
-import TopProfileCard from "../components/Profile/TopProfileCard.jsx"
+import ProfileCard from "../components/Profile/ProfileCard.jsx"
 import users from "../userdata.json"
 const Home = () => {
     return (
@@ -7,12 +7,28 @@ const Home = () => {
             <HeroSection />
             <section className="my-32 bg-[url(assets/TopProfileShape.png)] bg-center bg-cover">
                 <div>
-                    <h2 className="text-xl font-semibold">Top Profile</h2>
+                    <h2 className="text-xl font-semibold">Top Profiles</h2>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-20 mt-10">
                     {
                         users ? users.slice(0,3).map((user, idx)=>(
-                            <TopProfileCard 
+                            <ProfileCard 
+                                user={user}
+                                id={idx}
+                            />
+                            )) : <h2>No Top Profile yet</h2>
+
+                    }
+                </div>
+            </section>
+            <section className="my-32 bg-[url(assets/TopProfileShape.png)] bg-center bg-cover">
+                <div>
+                    <h2 className="text-xl font-semibold">Profiles</h2>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-20 mt-10">
+                    {
+                        users ? users.map((user, idx)=>(
+                            <ProfileCard 
                                 user={user}
                                 id={idx}
                             />
