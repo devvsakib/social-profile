96d5935cdeb8aa15dfa80aeff23f72f9b44072c7
DevvSakib <devvsakib@gmail.com>
2023-03-20 18:44:18 +0600

    favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-     
+
     </div>
   )
 }
