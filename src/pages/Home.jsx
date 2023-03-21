4b6f09e1688a53fcfcfde6887f904c7cbf5a2544
DevvSakib <devvsakib@gmail.com>
2023-03-21 22:44:25 +0600

    profile bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,34 +5,36 @@ const Home = () => {
     return (
         <section className="">
             <HeroSection />
-            <section className="my-32 bg-[url(/assets/TopProfileShape.png)] bg-center bg-cover">
+            <section className="my-32 bg-center bg-cover">
+                <div className="bg-[url(/assets/TopProfileShape.png)] bg-no-repeat bg-cover tpProfileBG"></div>
                 <div>
                     <h2 className="text-xl font-semibold">Top Profiles</h2>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10">
                     {
-                        users ? users.slice(0,3).map((user, idx)=>(
-                            <ProfileCard 
+                        users ? users.slice(0, 3).map((user, idx) => (
+                            <ProfileCard
                                 user={user}
                                 id={idx}
                             />
-                            )) : <h2>No Top Profile yet</h2>
+                        )) : <h2>No Top Profile yet</h2>
 
                     }
                 </div>
             </section>
-            <section className="my-32 bg-[url(/assets/ProfileShape.png)] bg-center bg-cover">
+            <section className="my-32 bg-center bg-cover relative">
+                <div className="bg-[url(/assets/TopProfileShape.png)] bg-no-repeat bg-cover ProfileBG"></div>
                 <div>
                     <h2 className="text-xl font-semibold">Profiles</h2>
                 </div>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10">
                     {
-                        users ? users.map((user, idx)=>(
-                            <ProfileCard 
+                        users ? users.map((user, idx) => (
+                            <ProfileCard
                                 user={user}
                                 id={idx}
                             />
-                            )) : <h2>No Top Profile yet</h2>
+                        )) : <h2>No Top Profile yet</h2>
 
                     }
                 </div>
