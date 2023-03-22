2d4f3bcb109378c56b863efbd5c000ba63c49488
DevvSakib <devvsakib@gmail.com>
2023-03-23 00:14:40 +0600

    banner bg opacity

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
   const social = ["twitter", "facebook", "gitHub", "linkedin", "instagram"]
   return (
     <section className="bg-[url(/assets/BannerShape.png)] bg-cover bg-center mt-10 xxl:w-[85vw]">
-      <div className="rounded-lg  p-5 pb-10 bnrcard-shadow bannerCard">
+      <div className="rounded-lg bg-white/10 p-5 pb-10 bnrcard-shadow bannerCard">
         <div className="text-center w-[]">
           <h1 className="font-sptitle tt text-4xl mt-5 break-words md:text-7xl">Share your Profile</h1>
           <div className="grid grid-cols-3 md:grid-cols-5 capitalize place-items-center justify-center my-8 gap-5">
