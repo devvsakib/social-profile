6528f7dea48207f9afe06767d0dffaed26fc9fea
DevvSakib <devvsakib@gmail.com>
2023-03-22 22:29:31 +0600

    about content added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,15 @@ import Layout from '../components'
 const About = () => {
   return (
     <Layout>
+      <div className='grid gap-5 my-20 md:text-xl'>
+        <p><span className='bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-600 font-semibold'>SocialProfile</span> is a platform that allows users to share their social media links and create a user profile. Whether you are a freelancer, an influencer, or a business owner, <span className='bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-600 font-semibold'>SocialProfile</span> provides an easy and convenient way to connect with your audience and showcase your online presence. </p>
+        
+        <p>Creating a user profile on Social Profiles is quick and easy. Once you sign up, you can add all your social media links to your profile, including links to your LinkedIn, Twitter, Facebook, GitHub, and Instagram accounts. You can also add a profile picture and a short bio to give your audience a better idea of who you are and what you do. </p>
+        
+        <p>With <span className='bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-600 font-semibold'>SocialProfile</span>, you can easily share your profile with others, whether it's through social media, email, or a personal website. This allows your audience to easily find and follow you on all your social media channels, helping you to grow your online presence and connect with more people. </p>
+        
+        <p>Whether you are looking to expand your personal brand or grow your business, <span className='bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-violet-600 font-semibold'>SocialProfile</span> provides a simple and effective way to manage your online presence and connect with your audience. Sign up today and start sharing your social media links and user profile with the world! </p>
+      </div>
     </Layout>
   )
 }
